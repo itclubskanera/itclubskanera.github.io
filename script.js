ce6066f82bb5df13a9bc0911a6bd59c5955500ba
DillonAdriyan <dillon.adrian00@gmail.com>
2024-02-24 23:32:44 +0700

    add shadow, animation, and fix navbar

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
   navbarToggle.addEventListener('click', function () {
     navbarLinks.classList.toggle('show');
     // Set max-height to auto when links are shown
-    navbarLinks.style.maxHeight = navbarLinks.classList.contains('show') ? '200px' : '0';
+    //navbarLinks.style.maxHeight = navbarLinks.classList.contains('show') ? '200px' : '0';
   });
 });
 // animated gsap text
